a688acf7e1df1e0496dfa897bd96ddb1423bd876
MuchtarHasan758 <muchtarhasan@proton.me>
2023-06-16 18:18:00 +1400

    menambahkan smooth scrolling di section-1 dan section-2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const sectionSatuBtn = document.querySelector("#section-satu-btn");
+const sectionDuaBtn = document.querySelector("#section-dua-btn");
+const sectionSatu = document.querySelector("#section-1");
+const sectionDua = document.querySelector("#section-2");
+
+sectionSatuBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  sectionSatu.scrollIntoView({ behavior: "smooth" });
+});
+
+sectionDuaBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  sectionDua.scrollIntoView({ behavior: "smooth" });
+});
